5648ce23b7a5cb9c284dcf87bab1856e82747df0
Andebrhan <anteforedu@gmail.com>
2023-03-24 23:20:53 -0400

    create project view added

---
+++ ticketing-project-mvc/src/main/java/com/cydeo/enums/Status.java
@@ -0,0 +1,11 @@
+package com.cydeo.enums;
+
+public enum Status {
+    OPEN("Open"), IN_PROGRESS("In Progress"), COMPLETE("Complete");
+
+    private final String value;
+
+    Status(String value) {
+        this.value = value;
+    }
+}
---
+++ ticketing-project-mvc/src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,33 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.service.ProjectService;
+
+import java.util.List;
+
+public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String> implements ProjectService {
+    @Override
+    public ProjectDTO save(ProjectDTO code) {
+        return super.save(code.getProjectCode(),code);
+    }
+
+    @Override
+    public List<ProjectDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public ProjectDTO findById(String id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(String id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void update(ProjectDTO name) {
+        super.update(name.getProjectName(),name);
+    }
+}
