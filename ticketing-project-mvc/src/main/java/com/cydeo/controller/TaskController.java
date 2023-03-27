0d62aee63af303ac2aa5d646857c459f55246ba1
Andebrhan <anteforedu@gmail.com>
2023-03-26 23:58:27 -0400

    task controller, dto, and converter added

---
+++ ticketing-project-mvc/src/main/java/com/cydeo/controller/TaskController.java
@@ -0,0 +1,18 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.TaskDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/create")
+public class TaskController {
+
+    @GetMapping("/create")
+    public String createTask(Model model) {
+        model.addAttribute("task", new TaskDTO());
+        return "task/create";
+    }
+}
