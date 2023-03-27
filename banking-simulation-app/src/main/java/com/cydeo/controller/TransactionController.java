ff11fdd74771edcb57f2483616392654adc429d1
Andebrhan <anteforedu@gmail.com>
2023-03-27 02:56:49 -0400

    name changed

--- banking-simulation-app/src/main/java/com/cydeo/controller/TransactionController.java
+++ banking-simulation-app/src/main/java/com/cydeo/controller/TransactionController.java
@@ -5,16 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TransferController {
+public class TransactionController {
 
     private final TransactionService transactionService;
 
-    public TransferController(TransactionService transactionService) {
+    public TransactionController(TransactionService transactionService) {
         this.transactionService = transactionService;
     }
 
     @GetMapping("/make-transfer")
     public String getMakeTransfer() {
+        //make transfer happen
+        //provide empty transaction object
+        //make all accounts to receive and send
+        //display the last 10 list of transactions
         return "transaction/make-transfer";
     }
 }
