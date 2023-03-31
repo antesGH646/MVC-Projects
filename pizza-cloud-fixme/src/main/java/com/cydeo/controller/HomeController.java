e176da04e161a75f2d76964d03e3e7811e7ea298
Andebrhan <anteforedu@gmail.com>
2023-03-31 17:37:51 -0400

    controller method added

--- pizza-cloud-fixme/src/main/java/com/cydeo/controller/HomeController.java
+++ pizza-cloud-fixme/src/main/java/com/cydeo/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
+    //error: this method was missing, not the home page displays
+    @RequestMapping("/home")
+    public String homePage() {
+        return "home";
+    }
 }
