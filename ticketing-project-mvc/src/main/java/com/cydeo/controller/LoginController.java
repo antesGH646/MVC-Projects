32dcaec0c6abe873766837e543c14c91611aecc5
Andebrhan <anteforedu@gmail.com>
2023-04-03 02:43:26 -0400

    formatted

--- ticketing-project-mvc/src/main/java/com/cydeo/controller/LoginController.java
+++ ticketing-project-mvc/src/main/java/com/cydeo/controller/LoginController.java
@@ -5,16 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
+
     //login endpoint created localhost:8080/login or localhost:8080/
     @RequestMapping(value = {"/login","/"})
     public String login(){
-
         return "login";
     }
+
     //directed to the welcome endpoint after the login button is clicked (th:action="@{/welcome}")
     @RequestMapping("/welcome")
     public String welcome(){
         return "welcome";
     }
-
 }
