ca8b3f2b066e4d5ce2a026af37204bd4485315d1
Andebrhan <anteforedu@gmail.com>
2023-04-19 21:13:27 -0400

    relationship built

--- ecommerce_app/src/main/java/com/cydeo/entity/Orders.java
+++ ecommerce_app/src/main/java/com/cydeo/entity/Orders.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import java.math.BigDecimal;
 
 @NoArgsConstructor
@@ -16,4 +17,7 @@ public class Orders extends BaseEntity{
 
     @ManyToOne
     private Customer customer;
+
+    @OneToOne
+    private Cart cart;
 }
