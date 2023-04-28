da01654b2e6daff84a17c80556dbb0ad3904b8bd
Andebrhan <anteforedu@gmail.com>
2023-04-28 14:15:38 -0400

    ticketing orm created

---
+++ ticketing-orm-project/src/main/java/com/cydeo/TicketingOrmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingOrmProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingOrmProjectApplication.class, args);
+    }
+
+}
