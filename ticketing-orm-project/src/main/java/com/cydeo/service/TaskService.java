1ec56894ee83f63caf8f9f28b46c18a09b50e9fe
Andebrhan <anteforedu@gmail.com>
2023-05-05 18:47:51 -0400

    Task started

--- ticketing-orm-project/src/main/java/com/cydeo/service/TaskService.java
+++ ticketing-orm-project/src/main/java/com/cydeo/service/TaskService.java
@@ -2,18 +2,14 @@ package com.cydeo.service;
 
 import com.cydeo.dto.TaskDTO;
 import com.cydeo.dto.UserDTO;
-import com.cydeo.enums.Status;
 
 import java.util.List;
 
-public interface TaskService extends CrudService<TaskDTO, Long> {
+public interface TaskService {
 
-    List<TaskDTO> findTasksByManager(UserDTO manager);
-
-    List<TaskDTO> findAllTasksByStatus(Status status);
-
-    List<TaskDTO> findAllTasksByStatusIsNot(Status status);
-
-    void updateStatus(TaskDTO task);
-
+    TaskDTO findById(Long id); //to find a task by id
+    List<TaskDTO> findAllTasks(UserDTO manager);
+    void save(TaskDTO dto);
+    void update(TaskDTO dto);
+    void delete(TaskDTO dto);
 }
