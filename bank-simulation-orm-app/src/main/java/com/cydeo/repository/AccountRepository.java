2d79ff81ce5c7b168732b0e737f04f52c4ede219
Andebrhan <anteforedu@gmail.com>
2023-05-11 00:17:13 -0400

    repositories prepared

--- bank-simulation-orm-app/src/main/java/com/cydeo/repository/AccountRepository.java
+++ bank-simulation-orm-app/src/main/java/com/cydeo/repository/AccountRepository.java
@@ -1,47 +1,8 @@
 package com.cydeo.repository;
 
-import com.cydeo.exception.RecordNotFoundException;
-import com.cydeo.dto.AccountDTO;
-import org.springframework.stereotype.Component;
+import com.cydeo.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+public interface AccountRepository extends JpaRepository<Account, Long> {
 
-@Component
-public class AccountRepository {
-    //stores list of account type
-    public static List<AccountDTO> accountList = new ArrayList<>();
-
-    /**
-     * Adds an account into the list and returns it
-     * @param account AccountType
-     * @return the list of AccountType
-     */
-    public AccountDTO addAccount(AccountDTO account) {
-        accountList.add(account);//adds an account into the list
-        return account;//returns each added individual account
-    }
-
-    /**
-     * Holds individual accounts
-     * @return a list of accounts
-     */
-    public List<AccountDTO> findAllAccounts() {
-        return accountList; //returns the above added list of accounts
-    }
-
-    /**
-     * This method finds an account by an id.
-     * The target id must match or must exist within the list of the accounts
-     * otherwise it throws an exception
-     * @param id account id
-     * @return matching id
-     */
-    public AccountDTO findAccountById(Long id) {
-        //finds an account from the list of accounts by id, otherwise throws exception if it does not exist
-        return accountList.stream().filter(account -> account.getId().equals(id))
-                .findAny()
-                .orElseThrow(() -> new RecordNotFoundException("Account not exist in the database."));
-    }
 }
--- bank-simulation-orm-app/src/main/java/com/cydeo/repository/TransactionRepository.java
+++ bank-simulation-orm-app/src/main/java/com/cydeo/repository/TransactionRepository.java
@@ -1,60 +1,8 @@
 package com.cydeo.repository;
 
-import com.cydeo.dto.TransactionDTO;
-import org.springframework.stereotype.Component;
+import com.cydeo.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-@Component
-public class TransactionRepository{
-
-    //stores list of transactions
-    public static List<TransactionDTO> transactionDTOList = new ArrayList<>();
-
-    /**
-     * Adds a transaction into the list and returns it
-     * @param transactionDTO AccountType
-     * @return the list of AccountType
-     */
-    public TransactionDTO addTransaction(TransactionDTO transactionDTO){
-        transactionDTOList.add(transactionDTO);//adds the transaction into a list
-        return transactionDTO; //returns an individual transaction
-    }
-
-    /**
-     * finds individual transactions
-     * @return a list of transactions
-     */
-    public List<TransactionDTO> findAllTransactions() {
-        return transactionDTOList;//returns the above list of transactions
-    }
-
-    /**
-     * Finds the last 10 transactions in a sorting order
-     * based on the creation date
-     * @return a list of last 10 transactions
-     */
-    public List<TransactionDTO> findLast10Transactions() {
-        return transactionDTOList.stream()
-                .sorted(Comparator.comparing(TransactionDTO::getCreationDate).reversed())
-                .limit(10)
-                .collect(Collectors.toList());
-    }
-
-    /**
-     * If an account id is used by either a sender or a receiver,
-     * this method finds list of transactions by id, then returns the transactions of the id
-     * @param id UUID
-     * @return list of transactions
-     */
-    public List<TransactionDTO> findTransactionListById(UUID id) {
-        return transactionDTOList.stream()
-                .filter(transactionDTO -> transactionDTO.getSender().equals(id)
-                        || transactionDTO.getReceiver().equals(id))
-                .collect(Collectors.toList());
-    }
 }
