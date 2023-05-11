c17a35dc3d4c95b7ddc23bca934d154594423548
Andebrhan <anteforedu@gmail.com>
2023-05-11 00:18:54 -0400

    listAllAccounts fixed

--- bank-simulation-orm-app/src/main/java/com/cydeo/service/impl/AccountServiceImpl.java
+++ bank-simulation-orm-app/src/main/java/com/cydeo/service/impl/AccountServiceImpl.java
@@ -1,8 +1,10 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.entity.Account;
 import com.cydeo.enums.AccountStatus;
 import com.cydeo.enums.AccountType;
 import com.cydeo.dto.AccountDTO;
+import com.cydeo.mapper.AccountMapper;
 import com.cydeo.repository.AccountRepository;
 import com.cydeo.service.AccountService;
 import org.springframework.stereotype.Component;
@@ -10,7 +12,7 @@ import org.springframework.stereotype.Component;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 public class AccountServiceImpl implements AccountService {
@@ -18,11 +20,13 @@ public class AccountServiceImpl implements AccountService {
 
     //private final forces to create a constructor, must add @component annotation on both classes
     //this class and the AccountRepository
-   private final AccountRepository accountRepository;
+    private final AccountRepository accountRepository;
+    private final AccountMapper accountMapper;
 
    //injecting by a constructor, takes the AccountRepository as a parameter
-    public AccountServiceImpl(AccountRepository accountRepository) {
+    public AccountServiceImpl(AccountRepository accountRepository, AccountMapper accountMapper) {
         this.accountRepository = accountRepository;
+        this.accountMapper = accountMapper;
     }
 
     /**
@@ -38,56 +42,63 @@ public class AccountServiceImpl implements AccountService {
     public AccountDTO createNewAccount(BigDecimal balance, Date creationDate,
                                        AccountType accountType, Long userId) {
        //create an account object
-        AccountDTO account = AccountDTO.builder().id(UUID.randomUUID())
-                .userId(userId).balance(balance).accountType(accountType).creationDate(creationDate)
-                .accountStatus(AccountStatus.ACTIVE).build();
-        //saving the account object into the database
-
-        //returning the created account object
-
-        return accountRepository.addAccount(account);
+        AccountDTO accountDTO = new AccountDTO();
+        return accountRepository.save(accountDTO);
     }
 
     /**
-     * This method returns a list of all accounts
-     * @return list of all accounts
+     * Since this method is returning list of AccountDTO
+     * The list of accounts that are fetched from the db
+     * must be converted to DTOs then returned
+     * @return list of accounts fetched from the db and converted to STOs
      */
     @Override
     public List<AccountDTO> listAllAccounts() {
-        return accountRepository.findAllAccounts();
+        List<Account> accountList = accountRepository.findAll();
+        return accountList.stream().map(accountMapper::convertToDTO).collect(Collectors.toList());
     }
 
-    /**
-     * This method deletes an account
-     * @param id UUID
-     */
+    @Override
+    public List<AccountDTO> listAllAccount() {
+
+        /*
+            we are getting list of account from repo(database
+            but we need to return list of AccountDTO to controller
+            what we need to do is we will convert Accounts to AccountsDTO
+         */
+
+        List<Account> accountList = accountRepository.findAll();
+        //we are converting list of account to accountDTOs and returning it.
+        return accountList.stream().map(accountMapper::convertToDTO).collect(Collectors.toList());
+    }
+
     @Override
     public void deleteAccount(Long id) {
         //find the account object based on id
-        AccountDTO account = accountRepository.findAccountById(id);
+        AccountDTO accountDTO = accountRepository.findById(id);
         //update the accountStatus of that object.
-        account.setAccountStatus(AccountStatus.DELETED);
+        accountDTO.setAccountStatus(AccountStatus.DELETED);
     }
 
-    /**
-     * This method activates a deleted account
-     * @param id UUID
-     */
     @Override
     public void activateAccount(Long id) {
         //find the account object based on id
-        AccountDTO account = accountRepository.findAccountById(id);
+        AccountDTO accountDTO = accountRepository.findById(id);
+
         //update the accountStatus of that object.
-        account.setAccountStatus(AccountStatus.ACTIVE);
+        accountDTO.setAccountStatus(AccountStatus.ACTIVE);
+
     }
 
-    /**
-     * This method get an account by its id
-     * @param id UUID
-     * @return returns an Account by its id
-     */
     @Override
     public AccountDTO retrieveAccountById(Long id) {
-        return accountRepository.findAccountById(id);
+        return null;
+    }
+
+    @Override
+    public AccountDTO retrieveById(Long id) {
+
+        return accountRepository.findById(id);
+    }
     }
 }
