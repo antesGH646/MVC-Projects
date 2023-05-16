14767c4a2c0ba099a71df07cba855cfa543bc698
Andebrhan <anteforedu@gmail.com>
2023-05-16 16:45:57 -0400

    annotation added

--- bank-simulation-orm-app/src/main/java/com/cydeo/repository/TransactionRepository.java
+++ bank-simulation-orm-app/src/main/java/com/cydeo/repository/TransactionRepository.java
@@ -3,10 +3,12 @@ package com.cydeo.repository;
 import com.cydeo.dto.TransactionDTO;
 import com.cydeo.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     List<TransactionDTO> findAllTransactions();
