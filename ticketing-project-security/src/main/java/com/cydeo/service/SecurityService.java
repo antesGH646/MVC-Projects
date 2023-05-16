2acdf7a209dfa13d52cdafc895b74cb9cb73089b
Andebrhan <anteforedu@gmail.com>
2023-05-16 16:42:17 -0400

    edited

--- ticketing-project-security/src/main/java/com/cydeo/service/SecurityService.java
+++ ticketing-project-security/src/main/java/com/cydeo/service/SecurityService.java
@@ -6,7 +6,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
  * Security services should extend the UserDetailsService,
  * because the UserDetailsService will automatically authenticate
  * the user in the UI
- * The SecurityServiceImpl implements the loadUserByUsername() method
+ * The SecurityServiceImpl Overrides to implement the
+ * loadUserByUsername() method
  */
 public interface SecurityService extends UserDetailsService {
 }
