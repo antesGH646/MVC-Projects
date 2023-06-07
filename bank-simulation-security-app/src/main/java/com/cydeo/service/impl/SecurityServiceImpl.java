5945ff18517602307a0d792b24eb1c38649392b4
Andebrhan <anteforedu@gmail.com>
2023-06-07 13:27:53 -0400

    implementing securityService

--- bank-simulation-security-app/src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
+++ bank-simulation-security-app/src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
@@ -1,12 +1,31 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.entity.User;
+import com.cydeo.entity.common.UserPrincipal;
+import com.cydeo.repository.UserRepository;
 import com.cydeo.service.SecurityService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SecurityServiceImpl implements SecurityService {
+    private final UserRepository userRepository;
+
+    public SecurityServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+       //get our own user from the db
+           User user = userRepository.findUserByUsername(username);
+        //return an exception if the specified user don't exist in the db
+        if(user==null){
+            throw new UsernameNotFoundException("This user does not exist");
+        }
+        //return user information as a UserDetails
+        return new UserPrincipal(user);
     }
 }
