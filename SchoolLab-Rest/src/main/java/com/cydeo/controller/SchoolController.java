bc761ae0ad24c77e9478438a65135166b5561516
Andebrhan <anteforedu@gmail.com>
2023-06-08 01:17:33 -0400

    parents endpoint created

--- SchoolLab-Rest/src/main/java/com/cydeo/controller/SchoolController.java
+++ SchoolLab-Rest/src/main/java/com/cydeo/controller/SchoolController.java
@@ -2,8 +2,10 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.ParentService;
 import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +17,11 @@ public class SchoolController {
 
     private final TeacherService teacherService;
     private final StudentService studentService;
-    public SchoolController(TeacherService teacherService, StudentService studentService) {
+    private final ParentService parentService;
+    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService) {
         this.teacherService = teacherService;
         this.studentService = studentService;
+        this.parentService = parentService;
     }
 
     /**
@@ -42,6 +46,23 @@ public class SchoolController {
     public ResponseEntity<ResponseWrapper> getAllStudents() {
         return ResponseEntity.ok(new ResponseWrapper("Students are successfully retrieved",
                 studentService.findAll()));
+    }
+
+    /**
+     * Creating an endpoint for parents
+     * responding message: "Parents are successfully retrieved"
+     * code: 202
+     * success: true
+     * parents data
+     */
+    @GetMapping("/parents")
+    public ResponseEntity<ResponseWrapper> getAllParents() {
+        ResponseWrapper responseWrapper = new ResponseWrapper(true, "Parents are successfully retrieved",
+                HttpStatus.ACCEPTED.value(), parentService.findAll());
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("Parent", "Returned")
+                .body(responseWrapper);
     }
 
 }
