d2f892cf6d263ff5f85c04f30e81ff141eb3a6b4
Andebrhan <anteforedu@gmail.com>
2023-06-08 21:49:03 -0400

    instead of hard coding username comes from keycloak

--- ticketing-project-rest/src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ ticketing-project-rest/src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -144,7 +144,15 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public List<TaskDTO> listAllTasksByStatus(Status status) {
-        User loggedInUser = userRepository.findByUserName("john@employee.com");
+        //username is hard coded
+        // User loggedInUser = userRepository.findByUserName("john@employee.com");
+
+        //instead of hard coding username needs to be caught from keycloak
+        Authentication authentication  = SecurityContextHolder.getContext().getAuthentication();
+        SimpleKeycloakAccount details = (SimpleKeycloakAccount) authentication.getDetails();
+        String username = details.getKeycloakSecurityContext().getToken().getPreferredUsername();
+        User loggedInUser = userRepository.findByUserName(username);
+
         List<Task> list = taskRepository.findAllByTaskStatusAndAssignedEmployee(status, loggedInUser);
         return list.stream().map(taskMapper::convertToDTO).collect(Collectors.toList());
     }
