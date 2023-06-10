5a86d00228444adc66c1beb9ac8fa72523017aef
Andebrhan <anteforedu@gmail.com>
2023-06-09 21:15:18 -0400

    edited

--- ticketing-project-rest-auto/src/main/java/com/cydeo/TicketingProjectRestAutoApplication.java
+++ ticketing-project-rest-auto/src/main/java/com/cydeo/TicketingProjectRestAutoApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class TicketingProjectRestApplication {
+public class TicketingProjectRestAutoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TicketingProjectRestApplication.class, args);
+		SpringApplication.run(TicketingProjectRestAutoApplication.class, args);
 	}
 
 	@Bean
