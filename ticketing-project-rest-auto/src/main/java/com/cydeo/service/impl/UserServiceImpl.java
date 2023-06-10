f2e176482df7e94993c4925aeae16801d5a7d1fb
Andebrhan <anteforedu@gmail.com>
2023-06-09 23:49:42 -0400

    custom exception added

--- ticketing-project-rest-auto/src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ ticketing-project-rest-auto/src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.TaskDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.User;
+import com.cydeo.exception.TicketingProjectException;
 import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.UserRepository;
 import com.cydeo.service.KeycloakService;
@@ -78,7 +79,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void delete(String username) {
+    public void delete(String username) throws TicketingProjectException {
         User user = userRepository.findByUserName(username);
 
         if (checkIfUserCanBeDeleted(user)) {
@@ -86,6 +87,9 @@ public class UserServiceImpl implements UserService {
             user.setUserName(user.getUserName() + "-" + user.getId());
             userRepository.save(user);
             keycloakService.delete(username);
+        }
+        else {
+            throw new TicketingProjectException("User cannot be deleted");
         }
     }
 
