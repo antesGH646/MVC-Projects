361040785a1a5b737f59f937882fc8917c01ad69
Andebrhan <anteforedu@gmail.com>
2023-06-27 12:28:24 -0400

    name changed

--- ticketing-project-rest-testing/src/main/java/com/cydeo/TicketingProjectRestTestingApplication.java
+++ ticketing-project-rest-testing/src/main/java/com/cydeo/TicketingProjectRestTestingApplication.java
@@ -8,11 +8,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class TicketingProjectRestApplication {
+public class TicketingProjectRestTestingApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(TicketingProjectRestApplication.class, args);
+		SpringApplication.run(TicketingProjectRestTestingApplication.class, args);
 	}
 
 	@Bean
---
+++ ticketing-project-rest-testing/src/test/java/com/cydeo/controller/ProjectControllerTest.java
@@ -0,0 +1,16 @@
+package com.cydeo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProjectControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    //Test post method
+}
