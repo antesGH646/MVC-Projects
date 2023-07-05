e3a3a663706e14b4c15395d247da1a51747bbad6
Andebrhan <anteforedu@gmail.com>
2023-07-05 19:31:34 -0400

    unit testing initial

---
+++ ticketing-project-rest-unit-testing/src/main/java/com/cydeo/dto/ProjectDTO.java
@@ -0,0 +1,31 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.Status;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProjectDTO {
+
+    private Long id;
+    private String projectName;
+    private String projectCode;
+    private UserDTO assignedManager;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String projectDetail;
+    private Status projectStatus;
+
+    //to exclude it from the json body when requesting
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private int completeTaskCounts;
+    //to exclude it from the json body when requesting
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private int unfinishedTaskCounts;
+
+}
