bcd09d3d7696941edc136f2a39ea2157eb896777
Andebrhan <anteforedu@gmail.com>
2023-09-08 16:00:18 -0400

    edited

--- ticketing-project-mvc/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ ticketing-project-mvc/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -79,16 +79,19 @@ public class DataGenerator implements CommandLineRunner {
                 LocalDate.now(),LocalDate.now().plusDays(10),"Creating Database", Status.IN_PROGRESS);
         ProjectDTO project3 = new ProjectDTO("Spring Container","PR003",user1,
                 LocalDate.now(),LocalDate.now().plusDays(32),"Creating Container", Status.IN_PROGRESS);
+
         //adding projects into the map object using save method
         projectService.save(project1);
         projectService.save(project2);
         projectService.save(project3);
 
+        //creating task objects
         TaskDTO task1 = new TaskDTO(project1, user8, "Controller", "Request Mapping", Status.IN_PROGRESS, LocalDate.now().minusDays(4));
         TaskDTO task2 = new TaskDTO(project3, user3, "Configuration", "Database Connection", Status.COMPLETE, LocalDate.now().minusDays(12));
         TaskDTO task3 = new TaskDTO(project3, user6, "Mapping", "One-To-Many", Status.COMPLETE, LocalDate.now().minusDays(8));
         TaskDTO task4 = new TaskDTO(project2, user7, "Dependency Injection", "Autowired", Status.IN_PROGRESS, LocalDate.now().minusDays(20));
 
+        //saving the task data
         taskService.save(task1);
         taskService.save(task2);
         taskService.save(task3);
