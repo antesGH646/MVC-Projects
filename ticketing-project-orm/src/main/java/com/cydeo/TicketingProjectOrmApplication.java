63259809ca2c2be07824bff119aef1e123604c55
Andebrhan <anteforedu@gmail.com>
2023-09-11 14:52:07 -0400

    edited and modified

--- ticketing-project-orm/src/main/java/com/cydeo/TicketingProjectOrmApplication.java
+++ ticketing-project-orm/src/main/java/com/cydeo/TicketingProjectOrmApplication.java
@@ -1,13 +1,21 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TicketingProjectOrmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectOrmApplication.class, args);
+    }
+
+ //a method that returns an object of the mapper
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
     }
 
 }
